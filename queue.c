9bd164d1d66ed3d8f6352b2853ea91f6b255a699
yuanhenglee <yuanhenglee2000@gmail.com>
2024-11-02 23:31:17 +0800

    Add new/free implementation

--- queue.c
+++ queue.c
@@ -10,31 +10,72 @@
  *   cppcheck-suppress nullPointer
  */
 
+inline element_t *q_new_element(char *s)
+{
+    element_t *e = malloc(sizeof(element_t));
+    if (!e)
+        return NULL;
+    e->value = strdup(s);
+    return e;
+}
+
+inline void q_free_element(element_t *e)
+{
+    if (!e)
+        return;
+    test_free(e->value);
+    test_free(e);
+}
+
 
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    if (!head)
+        return NULL;
+    INIT_LIST_HEAD(head);
+    return head;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    element_t *it, *safe;
+    list_for_each_entry_safe (it, safe, head, list)
+        q_free_element(it);
+    test_free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    element_t *e = q_new_element(s);
+    if (!e)
+        return false;
+    list_add(&e->list, head);
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    element_t *e = q_new_element(s);
+    if (!e)
+        return false;
+    list_add_tail(&e->list, head);
     return true;
 }
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
+    if (!head || list_empty(head))
+        return NULL;
+
     return NULL;
 }
 
@@ -49,11 +90,11 @@ int q_size(struct list_head *head)
 {
     if (!head)
         return 0;
-    int len = 1;
-    while (head->next) {
+    int len = 0;
+    struct list_head *it;
+    list_for_each (it, head)
         len++;
-        head = head->next;
-    }
+
     return len;
 }
 
@@ -97,16 +138,16 @@ int q_ascend(struct list_head *head)
     return 0;
 }
 
-/* Remove every node which has a node with a strictly greater value anywhere to
- * the right side of it */
+/* Remove every node which has a node with a strictly greater value anywhere
+ * to the right side of it */
 int q_descend(struct list_head *head)
 {
     // https://leetcode.com/problems/remove-nodes-from-linked-list/
     return 0;
 }
 
-/* Merge all the queues into one sorted queue, which is in ascending/descending
- * order */
+/* Merge all the queues into one sorted queue, which is in
+ * ascending/descending order */
 int q_merge(struct list_head *head, bool descend)
 {
     // https://leetcode.com/problems/merge-k-sorted-lists/
